9a27671ba5636137c430592bd6b68749a0b5b2c5
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-03-18 00:25:30 +0300

    Add project files.

---
+++ Benchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+   public static void Main(string[] args)
+    {
+        // bencmark ef core
+        Console.WriteLine("hello world");
+        Console.ReadKey();
+    }
+}
